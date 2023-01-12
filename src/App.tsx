c59903d12d9a609151c5f126e14146b2af23a136
ktgoldstein <60076196+ktgoldstein@users.noreply.github.com>
2023-01-12 16:23:56 -0500

    Add comment and fix final bug
    Bug was misplaced quotation marks.

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
-      showGraph: false
+      showGraph: false //only want graph to show when user indicates, initialize to false
     };
   }
 
